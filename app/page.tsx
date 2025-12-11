168dc8a7d54426f9e4daa9479231b8b549753b9b
Marti <programacio@cibergueda.com>
2025-12-11 23:35:42 +0100

    boto

--- app/page.tsx
+++ app/page.tsx
@@ -141,7 +141,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 sm:gap-4">
               {packs.map((pack) => (
                 <Link
                   key={pack.id}
